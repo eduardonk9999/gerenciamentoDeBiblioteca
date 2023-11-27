b0129c6ebef7b339e374f559fc92db322251b8b4
Eduardo Silva <thiago.novak7@gmail.com>
2023-11-27 20:22:49 +0000

    class Livro

--- src/Livro.java
+++ src/Livro.java
@@ -1,7 +1,15 @@
 public class Livro {
+    private int id;
     private String titulo;
-
     private String autor;
+    private boolean disponivel;
 
-    private int numPaginas;
+    // Construtor
+    private Livro(int id, String titulo, String autor){
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+
+    }
 }
